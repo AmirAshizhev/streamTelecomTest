4ed3008bd42fc3a238438c60c163fca50c8345ec
Amir Ashizhev <amirashizhev@yandex.ru>
2023-05-31 18:24:57 +0300

    feat: darktheme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
-// import logo from './logo.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Popup from '../Popup/Popup';
 import Settings from '../Settings/Settings';
 import './App.css';
@@ -7,7 +6,23 @@ import './App.css';
 function App() {
 
   const [openedPopup, setOpenedPopup] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(()=>{
+    const themeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    setIsDarkMode(themeQuery.matches)
+
+    const handleThemeChanges = () => {
+      setIsDarkMode(themeQuery.matches)
+    }
+
+    themeQuery.addEventListener('change', handleThemeChanges);
+
+    return () => {
+      themeQuery.removeEventListener('change', handleThemeChanges);
+    };
+
+  },[])
 
   return (
     <div className={`App ${isDarkMode && 'App_dark'}`}>
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -9,28 +9,28 @@ function Settings({setOpenedPopup, darkMode}) {
   return (
     <div className={`settings ${darkMode && 'settings_dark'}`}>
       <form className='settings__form'>
-        <div className="settings__box">
+        <div className='settings__box'>
           <div className={`settings__mech ${darkMode && 'settings__mech_dark'}`}></div>
           <h2  className={`settings__title ${darkMode && 'settings__title_dark'}`}>Настройки</h2>
-          <span className="settings__span">Логин: 77777777777</span>
+          <span className='settings__span'>Логин: 77777777777</span>
         </div>
 
-        <div>
-          <a className="settings__link" href='#' onClick={handleClick}>
-            <p className="settings__link-text">Двухфакторная аутентификация</p>
-            <div className="settings__link-img"></div>
+        <div className='settings__auth'>
+          <a className='settings__link' href='#' onClick={handleClick}>
+            <p className='settings__link-text'>Двухфакторная аутентификация</p>
+            <div className='settings__link-img'></div>
           </a>
-          <span className="settings__span">Не подключена</span>
+          <span className='settings__span'>Не подключена</span>
         </div>
 
-        <a className="settings__link" href='#'>
-          <p className="settings__link-text">Настройки безопасности и API</p>
-          <div className="settings__link-img settings__link-img_arrow"></div>
+        <a className='settings__link' href='#'>
+          <p className='settings__link-text'>Настройки безопасности и API</p>
+          <div className='settings__link-img settings__link-img_arrow'></div>
         </a>
 
-        <a className="settings__link" href='#'>
-          <p className="settings__link-text">Все протоколы интеграции</p>
-          <div className="settings__link-img settings__link-img_arrow"></div>
+        <a className='settings__link' href='#'>
+          <p className='settings__link-text'>Все протоколы интеграции</p>
+          <div className='settings__link-img settings__link-img_arrow'></div>
         </a>
 
         <button className={`settings__btn ${darkMode && 'settings__btn_dark'}`} type='button'>Создать пароль для API</button>
